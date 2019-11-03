bbd91614bc75982812df59a03040419089d5899a
Theo Salzmann <theo.salzmann@sandstorm.de>
2019-11-03 11:12:27 +0100

    wip

--- src/Redux/Store/Githubrepos/index.ts
+++ src/Redux/Store/Githubrepos/index.ts
@@ -1,6 +1,5 @@
 import { createSlice, PayloadAction } from 'redux-starter-kit'
-import { combineEpics } from 'redux-observable'
-import { ofType } from '@martin_hotell/rex-tils'
+import { combineEpics, ofType } from 'redux-observable'
 import { mergeMap, map, filter } from 'rxjs/operators'
 import { ajax } from 'rxjs/ajax'
 import { ActionsUnion } from '@martin_hotell/rex-tils'
@@ -56,7 +55,7 @@ const githubRepos = createSlice({
 
 const fetchReposEpic: RootEpic = (action$) => action$
   .pipe(
-      filter((action) => action.type === githubRepos.actions.fetch.type),
+      ofType<githubRepos.actions.fetch.type>(githubRepos.actions.fetch.type),
       mergeMap((action: PayloadAction<ReposFetch>) => ajax
        .getJSON<Array<Repository>>(`https://api.github.com/users/${action.payload.user}/repos`)
        // .pipe(
